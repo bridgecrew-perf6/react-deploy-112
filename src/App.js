aff3339717dc2e88933d90ff162563bfb0f6cec3
Hanul Rheem <skyrheem@gmail.com>
2022-02-24 13:50:37 +1300

    another update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
 import './App.css';
-
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1>Hello reactjs</h1>
       <h2>author: hanul rheem</h2>
     </div>
---
+++ src/components/AutoView.js
@@ -0,0 +1,16 @@
+import {BrowserView, MobileView} from 'react-device-detect';
+
+const AutoView = () => {
+
+    return(
+        <>
+        <BrowserView>
+            <h1>This is rendered only in BrowserView</h1>
+        </BrowserView>
+        <MobileView>
+            <h1>This is rendered only on MobileView</h1>
+        </MobileView>
+        </>
+    )
+  
+}
