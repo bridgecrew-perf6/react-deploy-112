6a8e5373faaca40d241fdd9eee7b9ef34455d17f
Hanul Rheem <skyrheem@gmail.com>
2022-02-24 13:53:00 +1300

    another update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Header from './components/Header';
+import AutoView from './components/AutoView';
 function App() {
   return (
     <div className="App">
       <Header/>
+      <AutoView/>
       <h1>Hello reactjs</h1>
       <h2>author: hanul rheem</h2>
     </div>
